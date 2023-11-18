2d6688813e8670224f440d5bc0a5f5518480129d
jul1anaportela <jul1anaportela@outlook.com>
2023-11-18 16:19:28 -0300

    Atividade jQuery Mask :tada:

---
+++ atividade/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('#telefone').mask('(00) 00000-0000', {
+        placeholder: '(__) _____-____'
+    })
+
+    $('#cpf').mask('000.000.000-00', {
+        placeholder: '___.___.___-__'
+    })
+
+    $('#cep').mask('00000-000', {
+        placeholder: '_____-___'
+    })
+})
